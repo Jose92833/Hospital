13a4bca8d2db32e8c8dbfaddd6d109a253afd31b
RobertDLR03 <lobelto2003@gmail.com>
2025-09-23 12:04:17 -0600

    Update DoctorController.java

--- src/main/java/com/example/demo/controller/DoctorController.java
+++ src/main/java/com/example/demo/controller/DoctorController.java
@@ -1,12 +1,24 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.DoctorRequest;
 import com.example.demo.dto.DoctorResponse;
+import com.example.demo.model.Doctor;
 import com.example.demo.service.DoctorService;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 @RequestMapping("api/v1/doctors")
@@ -17,9 +29,29 @@ public class DoctorController {
     public DoctorController(DoctorService service) {
         this.service = service;
     }
-    @PostMapping
-    public DoctorResponse add(DoctorRequest doctorRequest){
- return service.add(doctorRequest);
+    @PostMapping("/doctor")
+    public DoctorResponse add(DoctorRequest doctorRequest) {
+        return service.add(doctorRequest);
+    }
+
+    @GetMapping("/doctor")
+    public List<Doctor> obtenerTodos() {
+        return service.Todos();
+    }
+
+    @GetMapping("/doctor/{id}")
+    public Doctor ObtenerDoctorId(@PathVariable UUID id) {
+        return service.obtenerDoctor(id);
+    }
+
+    @PutMapping("doctor/{id}")
+    public void ActualizarDoctor(@PathVariable UUID id, @RequestBody Doctor nuevoDoctor) {
+        service.ActualizarDoctor(id, nuevoDoctor);
+    }
+
+    @DeleteMapping("/doctor/{id}")
+    public void EliminarDoctor(@PathVariable UUID id) {
+        service.EliminarDoctor(id);
     }
     
 }
